8738051567c89cb9d0e399bc4769e562761fc410
Alok Srivastava <alokksrivas@gmail.com>
2013-07-09 08:06:29 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var out = fs.readFileSync('/home/ubuntu/bitstarter/index.html', 'utf8');
+  var out = fs.readFileSync('index.html', 'utf8');
   // response.send('Hello World2!');
   response.send(out);
 });
